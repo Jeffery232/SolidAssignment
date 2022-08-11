11d19933d4553e1071cd424e32b21d3568d204ea
Support <Support@WEMA-TRAINING-S>
2022-08-11 06:33:29 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace SolidAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape[] shapes =
+         {
+            new Square(5, "Square #1"),
+            new Circle(3, "Circle #1"),
+            new Rectangle( 4, 5, "Rectangle #1")
+        };
+
+            System.Console.WriteLine("Shapes Collection");
+            foreach (Shape s in shapes)
+            {
+                System.Console.WriteLine(s);
+            }
+        }
+    }
+    }
---
+++ Shape.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidAssignment
+{
+    public abstract class Shape
+    {
+        private string name;
+
+        public Shape(string s)
+        {
+            // calling the set accessor of the Id property.
+            Id = s;
+        }
+
+        public string Id
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+
+        // Area is a read-only property - only a get accessor is needed:
+        public abstract double Area
+        {
+            get;
+        }
+
+        public override string ToString()
+        {
+            return $"{Id} Area = {Area:F2}";
+        }
+    }
+
+    public class Square : Shape
+    {
+        private int side;
+
+        public Square(int side, string id)
+            : base(id)
+        {
+            this.side = side;
+        }
+
+        public override double Area
+        {
+            get
+            {
+                // Given the side, return the area of a square:
+                return side * side;
+            }
+        }
+    }
+
+    public class Circle : Shape
+    {
+        private int radius;
+
+        public Circle(int radius, string id)
+            : base(id)
+        {
+            this.radius = radius;
+        }
+
+        public override double Area
+        {
+            get
+            {
+                // Given the radius, return the area of a circle:
+                return radius * radius * Math.PI;
+            }
+        }
+    }
+
+    public class Rectangle : Shape
+    {
+        private int width;
+        private int height;
+
+        public Rectangle(int width, int height, string id)
+            : base(id)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public override double Area
+        {
+            get
+            {
+                // Given the width and height, return the area of a rectangle:
+                return width * height;
+            }
+        }
+    }
+}
+
